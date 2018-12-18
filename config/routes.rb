3b1b18e68755390a2c7efc4a60b554aa83b9b56b
bran31 <bran31@gmail.com>
2018-12-18 04:18:05 -0500

    Working on templet and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 	root 'pages#index'
 	get 'about' , to:'pages#about'
 	get 'pics' , to:'pages#pics'
+
+
+	resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
