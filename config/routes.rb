4744d18afb9e9028932a6d7b0591d1fccce2d05b
bran31 <bran31@gmail.com>
2018-12-18 03:40:28 -0500

    added articles db

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
 	root 'pages#index'
 	get 'about' , to:'pages#about'
+	get 'pics' , to:'pages#pics'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
