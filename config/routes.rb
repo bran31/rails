bce60ce799fe5798c44d067b2b900b5e7b8cb7b8
bran31 <bran31@gmail.com>
2018-12-25 03:51:27 -0500

    working with users

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 	resources :articles
 	get 'signup', to: 'users#new'
 	post 'users', to: 'users#create'
+
+	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
